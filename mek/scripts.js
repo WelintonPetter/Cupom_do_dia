f96a13c41ee716c8ba9ec394dbb02d6c9f407a7e
Welinton Petter <84553202+WelintonPetter@users.noreply.github.com>
2024-06-25 18:30:36 -0300

    v1

---
+++ mek/scripts.js
@@ -0,0 +1,36 @@
+function showCoupon(code) {
+    var modal = document.getElementById("myModal");
+    var couponCodeText = document.getElementById("couponCode");
+    couponCodeText.textContent = "Código: " + code;
+    modal.style.display = "block";
+}
+
+function copyCoupon() {
+    var couponCodeText = document.getElementById("couponCode").textContent.split(": ")[1];
+    navigator.clipboard.writeText(couponCodeText).then(function() {
+        alert('Cupom copiado: ' + couponCodeText);
+    }, function(err) {
+        console.error('Erro ao copiar o cupom: ', err);
+    });
+}
+
+var closeBtn = document.getElementsByClassName("close")[0];
+closeBtn.onclick = function() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById("myModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function updatePromoCount() {
+    var promoCount = document.getElementsByClassName("coupon").length;
+    document.getElementById("promoCount").textContent = promoCount;
+}
+
+// Update the promotion count on page load
+document.addEventListener("DOMContentLoaded", updatePromoCount);
